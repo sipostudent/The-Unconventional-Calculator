f629f71472c9f7781eba7bd61baa4815371f3c89
Sipo Charles <sipocharles@yahoo.co.uk>
2020-03-07 23:59:39 +0000

    Updated

---
+++ basics-assignment-1-start/assignment.js
@@ -0,0 +1,30 @@
+/*
+    1.  Create two variables: One that holds a fictional user input (a number of your choice) 
+        and one that holds no value initially (e.g. "result"). 
+*/
+const userInput = 20;
+let result;
+
+/*
+    2.  Set the result variable to 18 plus the value stored in user input. 
+*/
+result = 18 + userInput;
+
+/*
+    3.  Add three additional lines of code where you change the result variable 
+        again (by subtracting a value of your choice, multiplying it and dividing it). 
+*/
+result = result - 10;
+result = result * 2;
+result = result / 4;
+
+/*
+    4.  Think about the value stored in the user input variable you also 
+    created - did that value change? 
+*/
+
+/*
+    5.  alert() the result and the user input variables (in two separate alert() calls). 
+*/
+alert(result);
+alert(userInput);
